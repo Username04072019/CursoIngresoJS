4aedda3a2bfd0ab25cd70ff4db220640b819b855
lucCant <52534610+lucCant@users.noreply.github.com>
2019-07-12 09:28:09 -0300

    Add files via upload

--- jsInstruccionIF (6).js
+++ jsInstruccionIF (6).js
@@ -1,6 +1,6 @@
 function mostrar()
 {
-//tomo la edad  
+//tomo la edad
 var edad
 edad = document.getElementById("edad").value;
 edad = parseInt(edad);
@@ -13,6 +13,16 @@ if (edad <= 12) {
         alert("La persona es mayor de edad.")
     }
 
-
+/* 12/07/19: no hace falta aclarar dos veces. ej:
+  if (edad > 17) {
+    alert("adulto");
+  } else {
+    if (edad > 13) {
+      alert("adolescente");
+    } else {
+      alert("niño");
+    }
+  }
+*/
 
 }//FIN DE LA FUNCIÓN
