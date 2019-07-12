d8a3b3d5fd13af84064c8b0da7338f1507a2291e
lucCant <52534610+lucCant@users.noreply.github.com>
2019-07-12 10:59:39 -0300

    Add files via upload

---
+++ jsInstruccionIF (8).js
@@ -0,0 +1,14 @@
+function mostrar()
+{
+//tomo la edad
+  var edad
+  edad = document.getElementById("edad").value;
+  edad = parseInt(edad);
+  var estado
+  estado = document.getElementById("estadoCivil").value;
+
+  if (edad > 17 && estado == "Soltero") {
+    alert("Es soltero y no es menor.");
+  }
+
+}//FIN DE LA FUNCIÓN
